36cb4bdf97c2a2402a109174d11875eaba0fd79d
eqwertyry <123989323+eqwertyry@users.noreply.github.com>
2025-10-30 18:46:01 +0100

    Delete vercel/api directory

--- vercel/api/test.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(200).json({ 
-    message: '✅ Всё работает! API готов к разработке.',
-    status: 'success',
-    next_step: 'Теперь создай базу данных в Vercel'
-  });
-}
