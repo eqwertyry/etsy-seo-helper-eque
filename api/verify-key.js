174b60ca9ba05f81de1aa5e8186d712ec68e85e0
eqwertyry <123989323+eqwertyry@users.noreply.github.com>
2025-10-30 19:17:43 +0100

    Update verify-key.js

--- api/verify-key.js
+++ api/verify-key.js
@@ -1,19 +1,29 @@
 import { createClient } from '@vercel/postgres';
 
-const client = createClient({
-  connectionString: process.env.DATABASE_URL
-});
-
 export default async function handler(req, res) {
-  const { key } = req.body;
+  try {
+    const client = createClient({
+      connectionString: process.env.DATABASE_URL
+    });
+    await client.connect();
 
-  await client.connect();
+    const { key } = req.body;
 
-  const result = await client.query('SELECT * FROM keys WHERE key=$1', [key]);
+    if (!key) {
+      return res.status(400).json({ error: 'Ключ не передан' });
+    }
 
-  if (result.rowCount > 0) {
-    res.status(200).json({ valid: true });
-  } else {
-    res.status(404).json({ valid: false });
+    const result = await client.query('SELECT * FROM keys WHERE key=$1', [key]);
+
+    if (result.rowCount > 0) {
+      res.status(200).json({ valid: true });
+    } else {
+      res.status(404).json({ valid: false });
+    }
+
+    await client.end();
+  } catch (error) {
+    console.error(error); // Важно, чтобы видеть в логах
+    res.status(500).json({ error: 'Ошибка сервера' });
   }
 }
